bd28df1290348cf3531925b5afbdb1c8a18bb0f2
Partha Chy <partha030900@gmail.com>
2026-07-04 20:47:06 +0600

    Configure Express server and application middleware

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+
+
+
+import type { Application, Request, Response } from "express";
+import express from 'express';
+import cors from "cors";
+import { authRouter } from "./modules/auth/auth.route";
+import { issuesRouter } from "./modules/issues/issues.route";
+import globalErrorHandler from "./middleware/globalErrorHandler";
+
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+app.use( cors({
+     origin: "https://devpulse-partha00.vercel.app"}));
+
+app.use("/api/auth", authRouter);
+app.use("/api/issues", issuesRouter);
+
+ app.use(globalErrorHandler);
+
+
+app.get('/', (req: Request, res: Response) => {
+     res.status(200).json({
+          message: "DevPulse - Internal Tech Issue & Feature Tracker",
+          author: "Next Level WebDev"
+     })
+});
+
+export default app;
